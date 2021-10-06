a9bfb33a3c5e17184bddadc7a2a8f0bbab1e6114
Christian D Guzman Torres <christian.guzman1@upr.edu>
2021-10-06 11:27:23 -0400

    Fix Formatting

--- OS_Lab4/ProducerConsumerSemaphores.c
+++ OS_Lab4/ProducerConsumerSemaphores.c
@@ -1,49 +1,59 @@
-nclude <pthread.h>
+#include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #define BUFFER_SIZE 20
+
 int count = 0;
 int buffer [BUFFER_SIZE];
 pthread_t tid;
+
 void insert(int item){
        while (count == BUFFER_SIZE);
-          buffer[count] = item;
-          count++;
-             sleep(1); 
+       buffer[count] = item;
+       count++;
+       sleep(1); 
 }
+
 int remove_item(){
        int item;
-          while (count == 0);
-             item = buffer[count];
-             count--;
-                sleep(1); 
-                   return item;
+       while (count == 0);
+       item = buffer[count];
+       count--;
+       sleep(1); 
+       return item;
 }
+
 void * producer(void * param){
        int item;
-          while(1){
-                    item = rand() % BUFFER_SIZE ;
-                          insert(item);
-                                printf("inserted: %d\n", item);
-                                   }
+       while(1){
+            item = rand() % BUFFER_SIZE ;
+            insert(item);
+            printf("inserted: %d\n", item);
+        }
 }
+
 void * consumer(void * param){
        int item;
-          while(1){
-                    item = remove_item();
-                        printf("removed: %d\n", item);
-                           }
+        while(1){
+            item = remove_item();
+            printf("removed: %d\n", item);
+        }
 }
+
 int main(int argc, char * argv[])
 {
         int producers = atoi(argv[1]);
-            int consumers = atoi(argv[2]);
-                int i;
-                    for (i = 0; i < producers; i++)
-                               pthread_create(&tid, NULL, producer,NULL);
-                        for (i = 0; i < consumers; i++)
-                                   pthread_create(&tid, NULL, consumer, NULL); 
-                            pthread_join(tid,NULL);
+        int consumers = atoi(argv[2]);
+        int i;
+
+        for (i = 0; i < producers; i++)
+            pthread_create(&tid, NULL, producer,NULL);
+        
+        for (i = 0; i < consumers; i++)
+            pthread_create(&tid, NULL, consumer, NULL); 
+            
+        pthread_join(tid,NULL);
 }
 
